8a534d2f5a6bdf5a0101aa85b9783a9f8db7c38d
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-01-22 22:13:35 +0000

    Add Link and Pressable components to navigate to the question page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,16 @@
+import { Link } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 export default function index() {
   return (
     <View>
       <Text>page one</Text>
+      <Link href="/question" style={{ padding: 30, backgroundColor: "black" }}>
+        <Pressable>
+          <Text style={{ color: "white" }}>go to question</Text>
+        </Pressable>
+      </Link>
     </View>
   );
 }
