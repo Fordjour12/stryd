9a6bc824835556f3a973c72fa505d2d95375b585
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-02-02 08:12:56 +0000

    Refactor question component and add progress bar

--- app/(questions)/question.tsx
+++ app/(questions)/question.tsx
@@ -1,100 +1,126 @@
-import { db } from "@/constants/firebase";
-import data from "@/onboardingquestion.json";
-import { addDoc, collection, doc, setDoc } from "firebase/firestore";
-import { useState } from "react";
-import { Button, Text, TextInput, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import AnswersBox from "@/components/AnswersBox";
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
+import { questions } from "@/constants/onboardingquestion";
+import { Link } from "expo-router";
+import React, { useState } from "react";
+import {
+  Pressable,
+  ProgressBarAndroid,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
-const questions = data.questions;
-
-export default function Questionnaire() {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [answers, setAnswers] = useState<Record<string, string>>();
-  const [inputValue, setInputValue] = useState("");
-
+export default function Question() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  const [answers, setAnswers] = useState<Record<string, string>>({});
 
-  const handleOptionPress = async (option: any) => {
-    setSelectedOption(option);
-    if (!option.includes("please specify")) {
-      handleAnswer(option);
-    }
-  };
+  const progressBarLength = (currentQuestionIndex + 1) / questions.length;
 
-  const handleAnswer = async (answer: string) => {
-    const answerToQuestions = {
+  // TextInput
+  const [inputValue, setInputValue] = useState<string>("");
+
+  function handleInputBlur() {
+    handleAnswer(inputValue);
+    setInputValue("");
+  }
+
+  // answering the question
+  const handleAnswer = (answer: string) => {
+    const answerToQuestion = {
       ...answers,
       [questions[currentQuestionIndex].question]: answer,
     };
 
-    setAnswers(answerToQuestions);
-    setCurrentQuestionIndex(currentQuestionIndex + 1);
-    setSelectedOption(null); // reset the selected option
+    console.log(answerToQuestion);
 
-    try {
-      await setDoc(doc(db, "answers", "questionnaire"), answerToQuestions);
-    } catch (error) {
-      console.error("Error adding document: ", error);
-    }
+    setAnswers(answerToQuestion);
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+    setSelectedOption(null);
   };
 
-  const handleInputBlur = () => {
-    handleAnswer(inputValue);
-    setInputValue(""); // reset the TextInput value
+  const handleOptionPress = (option: string) => {
+    setSelectedOption(option);
+    if (!option.includes("Specify")) {
+      handleAnswer(option);
+    }
   };
 
   if (currentQuestionIndex >= questions.length) {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Thank you for answering the questions!</Text>
+
+        <Link href="/" asChild>
+          <Pressable>
+            <Text>Completed</Text>
+          </Pressable>
+        </Link>
       </View>
     );
   }
 
-  async function nogree() {
-    const docRef = await addDoc(collection(db, "cities"), {
-      name: "Tokyo",
-      country: "Japan",
-    });
-  }
-
   return (
-    <SafeAreaView>
-      <View>
-        <Text>{questions[currentQuestionIndex].question}</Text>
-        {/*{questions[currentQuestionIndex].options?.map((option) => (
-          <Button
-            key={option}
-            onPress={() => handleOptionPress(option)}
-            title={option}
+    <>
+      <View style={styles.container}>
+        <View>
+          <ProgressBarAndroid
+            styleAttr="Horizontal"
+            indeterminate={false}
+            progress={progressBarLength}
+            color={Colors.coral}
           />
-        ))} 
-        */}
+        </View>
+        <Text style={styles.questions}>
+          {questions[currentQuestionIndex].question}
+        </Text>
         {questions[currentQuestionIndex].options ? (
-          questions[currentQuestionIndex].options?.map((option: string) => (
-            <Button
-              key={option}
-              onPress={() => handleOptionPress(option)}
-              title={option}
-            />
-          ))
-        ) : (
-          <TextInput
-            value={inputValue}
-            onChangeText={setInputValue}
-            onBlur={handleInputBlur}
-            placeholder="Type your answer here"
-          />
-        )}
-        {selectedOption?.includes("please specify") && (
-          <TextInput
-            value={inputValue}
-            onChangeText={setInputValue}
-            onBlur={handleInputBlur}
-            placeholder="Please specify"
-          />
-        )}
+          <View
+            style={{
+              columnGap: 10,
+              flexDirection: "row",
+              flexWrap: "wrap",
+              justifyContent: "space-around",
+            }}
+          >
+            {questions[currentQuestionIndex].options?.map(
+              (option: string, index: number) => (
+                <AnswersBox
+                  key={option}
+                  option={option}
+                  image={questions[currentQuestionIndex].images?.[index]}
+                  onPress={() => handleOptionPress(option)}
+                />
+              )
+            )}
+          </View>
+        ) : null}
       </View>
-    </SafeAreaView>
+      {selectedOption?.includes("Specify") && (
+        <TextInput
+          style={[defaultStyles.inputField, { marginHorizontal: 10 }]}
+          value={inputValue}
+          onChangeText={setInputValue}
+          onBlur={handleInputBlur}
+          placeholder="Please Specify"
+        />
+      )}
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  questions: {
+    fontFamily: "Raleway-Bold",
+    fontSize: 20,
+    paddingBottom: 20,
+  },
+});
